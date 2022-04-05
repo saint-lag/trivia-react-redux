7229344edbcaf1e5f4e8eb5836163f0cf84c6ae5
Heloisa <heloisajacobi@gmail.com>
2022-04-05 15:31:17 -0300

    Requisito_01 - Configuração do redux

---
+++ src/reducers/player.js
@@ -0,0 +1,13 @@
+const INITIAL_STATE = {
+  name: '',
+  assertions: '',
+  score: '',
+  gravatarEmail: '',
+};
+
+export default function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
