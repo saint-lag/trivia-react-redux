30ce1c727568181d16cb3327d89c04406ee72391
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-04-07 16:13:11 -0300

    Requisito-13: Cria page Feedback.

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Feedback extends Component {
+  render() {
+    return (
+      <>
+        Feedback
+      </>
+    );
+  }
+}
+
+export default connect(null, null)(Feedback);
