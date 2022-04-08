18cbac3efc6a2ac4c96be680bc8f5bd9cc4540e3
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-04-08 16:01:43 -0300

    Requisito-17 => insere um button na tela do Ranking que retorna à tela de Login

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
   render() {
+    const { history } = this.props;
     return (
-      <h1 data-testid="ranking-title">TEMPORARY: Ranking Title</h1>
+      <>
+        <h1 data-testid="ranking-title">TEMPORARY: Ranking Title</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          Voltar à Login
+        </button>
+      </>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.node.isRequired,
+};
 
 export default Ranking;
