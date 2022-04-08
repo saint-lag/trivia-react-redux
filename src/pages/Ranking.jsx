637af092290582b2d41ead1491ba1bcf011c9e8e
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-04-08 18:24:49 -0300

    Req-15: Adiciona cards com as informações dos jogos anteriores no ranking.

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -2,11 +2,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      ranking: JSON.parse(localStorage.getItem('ranking')),
+    };
+  }
+
+  // comp
+
+  // loadGamesInfo = () => {
+  //   this.setState({
+  //     ranking: JSON.parse(localStorage.getItem('ranking')),
+  //   });
+  // }
+
   render() {
     const { history } = this.props;
+    const { ranking } = this.state;
+    console.log(ranking);
     return (
       <>
-        <h1 data-testid="ranking-title">TEMPORARY: Ranking Title</h1>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <section>
+          {ranking.sort((a, b) => b.score - a.score).map((game, index) => (
+            <div key={ `ranking${index}` }>
+              <img src={ game.picture } alt={ `Foto do ${game.name}` } />
+              <span data-testid={ `player-name-${index}` }>
+                {' '}
+                {game.name}
+                {' '}
+              </span>
+              <span data-testid={ `player-score-${index}` }>
+                {' '}
+                {game.score}
+                {' '}
+              </span>
+            </div>))}
+        </section>
         <button
           type="button"
           data-testid="btn-go-home"
